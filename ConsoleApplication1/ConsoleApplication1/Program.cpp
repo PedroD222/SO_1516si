4debd6a7d622d5fabf143b734a55eb49e2c7e25a
PedroD222 <pedroduarte222@hotmail.com>
2015-10-28 19:20:42 +0000

    show dll

--- ConsoleApplication1/ConsoleApplication1/Program.cpp
+++ ConsoleApplication1/ConsoleApplication1/Program.cpp
@@ -25,18 +25,34 @@ int _tmain(int argc, _TCHAR* argv[])
 	getModuleFilenameEX handle process,
 						hmodule - corresponde ao endereçoi base de mapeamento da dll via virtual query*/
 	MEMORY_BASIC_INFORMATION mbi;
-	PBYTE adress = 0x0;
+	PBYTE adress = NULL;
+	DWORD err = 0;
 	wchar_t name[256];
 	DWORD sizenamedll;
+	HMODULE hMod;
 	while (VirtualQueryEx(proc, adress, &mbi, sizeof(mbi))) {
-		if (mbi.State == MEM_COMMIT) {
-			sizenamedll = GetModuleFileNameEx(proc, (HMODULE)mbi.AllocationBase,  name, 256);
+		/*err = GetLastError();
+		if (err != 0)
+			_tprintf(_T("ERROR: %d \n"), err);*/
+		hMod = (HMODULE)mbi.AllocationBase;
+		if (mbi.State == MEM_FREE) {
+			hMod = (HMODULE)mbi.BaseAddress;
+			mbi.AllocationBase = mbi.BaseAddress;
+		}
+			
+		if (mbi.AllocationBase != NULL && mbi.BaseAddress == mbi.AllocationBase && mbi.State == MEM_COMMIT) {
+			sizenamedll = GetModuleFileNameEx(proc, hMod, name, 256);
 			if (sizenamedll != 0)
-				_tprintf(_T("Endereço Base: %d - %d - %s \n"), mbi.AllocationBase, mbi.RegionSize, name);
+				_tprintf(_T("Endereço Base: %d - Regiao %d - DLL: %s \n"), mbi.AllocationBase, mbi.RegionSize, name);
+			/*else {
+				_tprintf(_T("ERROR: %d \n"), GetLastError());
+			}*/
 		}
 		adress += mbi.RegionSize;
 	}
-	CloseHandle(proc);
+	int cl = CloseHandle(proc);
+	if (cl == 0)
+		_tprintf(_T("ERROR: %d \n"), GetLastError());
 	_tprintf(_T("Pressionar qualquer tecla para sair\n"));
 	getchar();
     return 0;
