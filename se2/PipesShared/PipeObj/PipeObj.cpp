e2ce96d47de98814b5ebec38e22a5979e9003fb2
PedroD222 <pedroduarte222@hotmail.com>
2016-01-18 18:53:22 +0000

    nn

--- se2/PipesShared/PipeObj/PipeObj.cpp
+++ se2/PipesShared/PipeObj/PipeObj.cpp
@@ -2,11 +2,30 @@
 //
 
 #include "stdafx.h"
-#include "../Pipe/Pipe.h"
+#include "PipeObj.h"
+//#include "../Pipe/Pipe.h"
+#include "../PipeService/PipeService.h"
+
+HANDLE LaunchProcess(TCHAR *cmdLine) {
+	PROCESS_INFORMATION pi;
+	STARTUPINFO si;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+
+	if (!CreateProcess(NULL, cmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+		return NULL;
+	}
+
+	CloseHandle(pi.hThread);
+	return pi.hProcess;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	PPIPE create = PipeCreate(_T("pipe"));
+	PPIPE create = PipeCreate(NAMEPIPE);
+	//HANDLE child2 = LaunchProcess(_T("PipeReader"));
+
 	getchar();
 	return 0;
 }
